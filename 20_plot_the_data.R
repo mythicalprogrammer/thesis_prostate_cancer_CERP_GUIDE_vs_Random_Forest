8d01eccb423d249930269b62bcc4e7842156fee3
Anthony Doan <mythicalprogrammer@gmail.com>
2018-12-20 15:10:57 -0800

    Change the title of the plot.

--- 20_plot_the_data.R
+++ 20_plot_the_data.R
@@ -1,4 +1,4 @@
-title <- "Finding Optimal Number of Partitions Using Leave One Out Cross-Validation"
+title <- "CERP-GUIDE Forest (unpruned) vs CERP-GUIDE Forest (pruned) vs Random Forest"
 ggplot(data = confusion_matrices, aes(x = partition_number, y = accuracy, group = group)) +
   geom_line(aes(linetype=group,color=group)) +
   geom_point(aes(shape=group,color=group)) +
