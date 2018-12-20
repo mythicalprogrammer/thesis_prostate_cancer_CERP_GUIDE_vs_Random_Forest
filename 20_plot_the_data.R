19b7a9ae2426fe5f784c31b9625fafe5b1411cdd
Anthony Doan <mythicalprogrammer@gmail.com>
2018-12-20 15:02:15 -0800

    added code to plot all the data.

---
+++ 20_plot_the_data.R
@@ -0,0 +1,6 @@
+title <- "Finding Optimal Number of Partitions Using Leave One Out Cross-Validation"
+ggplot(data = confusion_matrices, aes(x = partition_number, y = accuracy, group = group)) +
+  geom_line(aes(linetype=group,color=group)) +
+  geom_point(aes(shape=group,color=group)) +
+  xlab("Number of Parititions") + ylab("Accuracy") +
+  ggtitle(title)
