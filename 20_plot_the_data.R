8ed3c52cc3ec1d0b4b78c132690d2b4be89fa747
Anthony Doan <mythicalprogrammer@gmail.com>
2018-12-20 15:05:37 -0800

    Changed the x axis to correct label.

--- 20_plot_the_data.R
+++ 20_plot_the_data.R
@@ -2,5 +2,5 @@ title <- "Finding Optimal Number of Partitions Using Leave One Out Cross-Validat
 ggplot(data = confusion_matrices, aes(x = partition_number, y = accuracy, group = group)) +
   geom_line(aes(linetype=group,color=group)) +
   geom_point(aes(shape=group,color=group)) +
-  xlab("Number of Parititions") + ylab("Accuracy") +
+  xlab("Number of Trees") + ylab("Accuracy") +
   ggtitle(title)
